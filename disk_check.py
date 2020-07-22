5621c17d82b7d0c834246ba0f963eff40336f320
Atindra Shekhar <56440918+atindra305@users.noreply.github.com>
2020-07-22 14:30:38 +0530

    Add files via upload

---
+++ disk_check.py
@@ -0,0 +1,15 @@
+# Python program to explain shutil.disk_usage() method  
+    
+# importing shutil module  
+import shutil 
+  
+# Path 
+path = "/Users/Atindra Shekhar/Documents"
+  
+# Get the disk usage statistics 
+# about the given path 
+stat = shutil.disk_usage(path) 
+  
+# Print disk usage statistics 
+print("Disk usage statistics:") 
+print(stat) 
